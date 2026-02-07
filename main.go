0be87af777d523f3495c2b53ff26b64a59c0c833
frankheinz87 <frankheinz1987@gmail.com>
2026-02-07 18:43:44 +0100

    updated main.go to use login

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/frankheinz87/blogaggregator/internal/config"
 )
@@ -13,17 +14,38 @@ func main() {
 		return
 	}
 
-	err = cfg.SetUser("frank")
+	/*err = cfg.SetUser("frank")
 	if err != nil {
 		fmt.Println("error setting user:", err)
 		return
+	}*/
+
+	st := &state{cfg: &cfg}
+
+	cmds := commands{m: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("not enough arguments")
+		os.Exit(1)
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println("error reading file:", err)
-		return
+	name := args[1]
+	cmdArgs := []string{}
+	if len(args) > 2 {
+		cmdArgs = args[2:]
 	}
 
-	fmt.Printf("%+v\n", cfg)
+	cmd := command{
+		name: name,
+		args: cmdArgs,
+	}
+
+	if err := cmds.run(st, cmd); err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
 }
