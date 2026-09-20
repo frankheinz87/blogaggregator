22ed85e5da28753ed08893f864e1470306ab5a81
frankheinz87 <frankheinz1987@gmail.com>
2026-09-20 17:37:00 +0200

    added RSS structs and included response unscaping

--- rss_feed.go
+++ rss_feed.go
@@ -3,9 +3,26 @@ package main
 import (
 	"context"
 	"encoding/xml"
+	"html"
 	"io"
 	"net/http"
 )
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
 
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 
@@ -39,6 +56,14 @@ func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 
 	if err != nil {
 		return nil, err
+	}
+
+	result.Channel.Title = html.UnescapeString(result.Channel.Title)
+	result.Channel.Description = html.UnescapeString(result.Channel.Description)
+
+	for i := range result.Channel.Item {
+		result.Channel.Item[i].Title = html.UnescapeString(result.Channel.Item[i].Title)
+		result.Channel.Item[i].Description = html.UnescapeString(result.Channel.Item[i].Description)
 	}
 
 	return &result, nil
