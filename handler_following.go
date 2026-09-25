5c5f8421889bfe227e87e7d47c68ac6516d9407d
frankheinz87 <frankheinz1987@gmail.com>
2026-09-25 22:16:57 +0200

    implemented unfollow command

--- handler_following.go
+++ handler_following.go
@@ -27,3 +27,27 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 
 	return nil
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+
+	if len(cmd.args) < 1 {
+		return errors.New("feed url required")
+	}
+
+	feed, err := s.db.GetFeed(context.Background(), cmd.args[0])
+
+	if err != nil {
+		return err
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ sql/queries/delete_feed_follow.sql
@@ -0,0 +1,3 @@
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows
+WHERE feed_follows.user_id = $1 AND feed_follows.feed_id = $2;
